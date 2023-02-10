91afe29f16e18db6a5c4e84d85ecc0c45bd05371
elite3312 <perrywu12@gmail.com>
2023-02-10 22:19:22 +0800

    rename

--- rename.py
+++ rename.py
@@ -6,5 +6,5 @@ for dir in dirs:
     files=glob.glob(dir+'/*.py')
     for file in files:
         _base_name=os.path.basename(file)
-        _new_base_name=_base_name.replace("_"," ")
+        _new_base_name=_base_name.replace(" ","_")
         os.rename(file,file.replace(_base_name,_new_base_name))
